a43f92549f2bb8381482729a55af27eb74fcea3f
Christian Schauer <schauer@inf-schauer.de>
2022-09-18 18:09:43 +0200

    initial commit

---
+++ src/main/java/com/graphql/grapnel/GrapnelApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.grapnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapnelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphql/grapnel/GrapnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.grapnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
