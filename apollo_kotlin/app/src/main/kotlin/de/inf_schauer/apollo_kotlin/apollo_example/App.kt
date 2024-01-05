d5305478b781109b7e0e99846b568409af2b5111
Chris <schauer@inf-schauer.de>
2024-01-05 20:51:01 +0100

    First working query

--- apollo_kotlin/app/src/main/kotlin/de/inf_schauer/apollo_kotlin/apollo_example/App.kt
+++ apollo_kotlin/app/src/main/kotlin/de/inf_schauer/apollo_kotlin/apollo_example/App.kt
@@ -3,13 +3,51 @@
  */
 package de.inf_schauer.apollo_kotlin.apollo_example
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.network.okHttpClient
+import de.inf_schauer.apollo_kotlin.apollo_example.gql.DetailsQuery
+import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+
+class ApolloExample {
+    fun run() {
+        val apolloClient =
+            ApolloClient.Builder().serverUrl("http://localhost:8080/graphql").okHttpClient(OkHttpClient()).build()
+
+        val detailsQuery = DetailsQuery("4cfe25a2-529e-432e-be08-87ee9094774a")
+
+        val job = Job()
+        val coroutineScope = CoroutineScope(Dispatchers.Default + job)
+
+        val deferredResponse = coroutineScope.async {
+            apolloClient.query(detailsQuery).execute()
         }
+        runBlocking {
+            try {
+                val response = deferredResponse.await()
+                if (response.hasErrors()) {
+                    // Handle errors
+                    response.errors?.forEach { error ->
+                        println("GraphQL Error: ${error.message}")
+                    }
+                } else {
+                    // Handle successful response
+                    val product = response.data?.productById
+                    println("Product ID: ${product?.id}")
+                    println("Product Name: ${product?.name}")
+                    println("Product Number: ${product?.product_no}")
+                    val manufacturer = product?.manufacturer
+                    println("Manufacturer ID: ${manufacturer?.id}")
+                    println("Manufacturer Name: ${manufacturer?.name}")
+                }
+            } finally {
+                println("finally")
+                apolloClient.close()
+            }
+        }
+    }
 }
 
 fun main() {
-    println(App().greeting)
+    ApolloExample().run()
 }
