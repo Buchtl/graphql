15caea8dc75176777f93af8dbaf156483ec1f481
Chris <schauer@inf-schauer.de>
2024-01-19 18:36:09 +0100

    frontend: hello react

--- apollo_frontend/src/index.tsx
+++ apollo_frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloWorld from './HelloWorld';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <HelloWorld />
   </React.StrictMode>
 );
 
