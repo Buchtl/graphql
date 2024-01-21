fadd5c66f51419434d7e91e1be819ce0b731ef9c
Chris <schauer@inf-schauer.de>
2024-01-21 15:11:19 +0100

    Subscribtion part2 (#3)
    * basic subscription

--- apollo_frontend/src/ProductDynamicList.tsx
+++ apollo_frontend/src/ProductDynamicList.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState, useEffect } from "react";
-import { ApolloClient, InMemoryCache, gql,useSubscription } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql, useSubscription } from '@apollo/client';
+import { List, ListItem, ListItemText, Paper } from '@mui/material';
 
 
 interface Product {
@@ -19,9 +20,9 @@ const PRODUCT_SUBSCRIPTION = gql`
 
 function ProductDynamicList() {
   const [products, setProducts] = useState<Product[]>([{ id: "", name: "" }])
-  
-  const { data, loading} = useSubscription(PRODUCT_SUBSCRIPTION);
-  
+
+  const { data, loading } = useSubscription(PRODUCT_SUBSCRIPTION);
+
   const client = new ApolloClient({
     uri: 'http://localhost:8080/graphql',
     cache: new InMemoryCache()
@@ -41,24 +42,27 @@ function ProductDynamicList() {
     response.then(r => r.data).then(data => { setProducts(data.allProducts) })
   }, [])
 
-  /*useEffect(() => {
-    let tmp: Product = {id: data.productAdded.name, name: data.productAdded.name}
-    console.log("++++++++++++++++ " + tmp.name)
-  }, [data])*/
+  useEffect(() => {
+    if (data) {
+      setProducts([...products, { id: data.productAdded.id, name: data.productAdded.name }])
+    }
+  }, [data])
 
-
-  //<h2>{!loading && data.productAdded.name}</h2>
   return (
-    <div className="App-body">
-      <h2>Latest</h2>
-      <div>{!loading && data.productAdded.name}</div>
-      <h2>Products</h2>
+    <div className="App-products">
+      <h3>Products</h3>
+      <div>Recently added: {!loading && data.productAdded.name}</div>
       <table>
         <tbody>
           <tr><th>ID</th><th>Name</th></tr>
-          {products.map(product => <tr><td>{product.id}</td><td>{product.name}</td></tr>)}
+          {products.map(product => <tr><td>{product.id}</td><td>{product.name}</td></tr>).reverse()}
         </tbody>
       </table>
+      <Paper style={{ maxHeight: 200, overflow: 'auto' }}>
+        <List>
+          {products.map(product => <ListItem>{product.id}{product.name}</ListItem>)}
+        </List>
+      </Paper>
     </div>
   );
 }
