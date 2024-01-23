90986fa726d565014fe3cc45ca4edf7ec9fdef48
Chris <schauer@inf-schauer.de>
2024-01-23 19:19:43 +0100

    wip: footer

---
+++ apollo_frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+function Footer() {
+
+  return (
+    <footer>
+      Fooooooooooooooooooooooooter
+    </footer>
+  );
+}
+
+
+export default Footer;
--- apollo_frontend/src/components/Layout/Layout.tsx
+++ apollo_frontend/src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Layout.css";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 export default function Layout( {children} : any ) {
   return (
     <div>
       <Header/>
       {children}
+      <Footer/>
     </div>
   );
 }
